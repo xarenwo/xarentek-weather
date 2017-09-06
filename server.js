a9c82084eaf87e58354fab52905e5a6a8621c191
Andrei Maris <xarenwo@gmail.com>
2017-09-06 12:12:37 +0200

    Xarentek Weather 0.1

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ var express = require('express'); // access to express api
 var app = express(); // to create the app, call the express library
 var port = process.env.PORT || 3000;
 
+
+//redirect to http
+app.use(function (req,res,next) {
+if(req.headers['x-forwarded-proto'] === 'http') { //https or http
+  next(); // go on
+}  else {
+  res.redirect('http://'+ req.hostname + req.url);
+}
+});
+
+
 app.use(express.static('public'));
 app.listen(port, function(){
   console.log('Server deployed on port ' + port);
